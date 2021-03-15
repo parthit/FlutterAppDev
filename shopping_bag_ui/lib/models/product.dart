55e28586121348e694a66c51af48a2d1109d187c
Parthit Patel <parthitpatel@gmail.com>
2021-03-15 11:44:49 +0530

    Created Product class to hold bag items as list of objects

---
+++ shopping_bag_ui/lib/models/product.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class Product {
+  final String image, title, description;
+  final int price, size, id;
+  final Color color;
+
+  Product({this.color, this.price, this.title, this.size, this.image, this.id, this.description});
+
+}
+
+
+List<Product> products = [
+  Product(
+      id: 1,
+      title: "Office Code",
+      price: 234,
+      size: 12,
+      description: dummyText,
+      image: "assets/images/bag_1.png",
+      color: Color(0xFF3D82AE)),
+  Product(
+      id: 2,
+      title: "Belt Bag",
+      price: 234,
+      size: 8,
+      description: dummyText,
+      image: "assets/images/bag_2.png",
+      color: Color(0xFFD3A984)),
+  Product(
+      id: 3,
+      title: "Hang Top",
+      price: 234,
+      size: 10,
+      description: dummyText,
+      image: "assets/images/bag_3.png",
+      color: Color(0xFF989493)),
+  Product(
+      id: 4,
+      title: "Old Fashion",
+      price: 234,
+      size: 11,
+      description: dummyText,
+      image: "assets/images/bag_4.png",
+      color: Color(0xFFE6B398)),
+  Product(
+      id: 5,
+      title: "Office Code",
+      price: 234,
+      size: 12,
+      description: dummyText,
+      image: "assets/images/bag_5.png",
+      color: Color(0xFFFB7883)),
+  Product(
+    id: 6,
+    title: "Office Code",
+    price: 234,
+    size: 12,
+    description: dummyText,
+    image: "assets/images/bag_6.png",
+    color: Color(0xFFAEAEAE),
+  ),
+];
+
+String dummyText = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.";
