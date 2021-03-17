5b74846e6d6639a7d63633efd40d0438ff6d166a
Parthit Patel <parthitpatel@gmail.com>
2021-03-18 04:11:35 +0530

    created App Bar in details screen and created file for details body

---
+++ shopping_bag_ui/lib/screen/details/components/details_body.dart
@@ -0,0 +1,8 @@
+import "package:flutter/material.dart";
+
+class DetailsBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- shopping_bag_ui/lib/screen/details_screen.dart
+++ shopping_bag_ui/lib/screen/details_screen.dart
@@ -1,5 +1,9 @@
 import "package:flutter/material.dart";
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shopping_bag_ui/constants.dart';
 import 'package:shopping_bag_ui/models/product.dart';
+
+import 'details/components/details_body.dart';
 
 class DetailsScreen extends StatelessWidget {
   final Product product;
@@ -8,6 +12,31 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: product.color,
+      appBar: buildAppBar(context),
+      body: DetailsBody(),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: product.color,
+      leading: IconButton(
+        icon: SvgPicture.asset(
+          "assets/icons/back.svg",
+          color: Colors.white,
+        ),
+        onPressed: () => Navigator.pop(context),
+      ),
+
+      actions: <Widget>[
+        IconButton(icon: SvgPicture.asset("assets/icons/search.svg"), onPressed: (){}),
+        IconButton(icon: SvgPicture.asset("assets/icons/cart.svg"), onPressed: (){}),
+        SizedBox(width: kDefaultPadding/2),
+
+      ],
+    );
   }
 }
