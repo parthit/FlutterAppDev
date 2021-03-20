82964812d8dbe710e2c0dd42288cfedcea5b5477
Parthit Patel <parthitpatel@gmail.com>
2021-03-20 22:26:07 +0530

    Added increment and decrement buttons

--- shopping_bag_ui/lib/screen/details/components/details_body.dart
+++ shopping_bag_ui/lib/screen/details/components/details_body.dart
@@ -33,7 +33,8 @@ class DetailsBody extends StatelessWidget {
                   child: Column(
                     children: <Widget>[
                       ColorAndSize(product: product),
-                      Description(product: product)
+                      Description(product: product),
+                      CartCounter()
                     ],
                   ),
                 ),
@@ -42,6 +43,60 @@ class DetailsBody extends StatelessWidget {
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class CartCounter extends StatefulWidget {
+  @override
+  _CartCounterState createState() => _CartCounterState();
+}
+
+class _CartCounterState extends State<CartCounter> {
+  int numOfItems = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        buildOutlineButton(
+            icon: Icons.remove,
+            pressed: () {
+              if (numOfItems > 1) {
+                setState(() {
+                  numOfItems--;
+                });
+              }
+            }),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          child: Text(
+            numOfItems.toString().padLeft(2, "0"),
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ),
+        buildOutlineButton(
+            icon: Icons.add,
+            pressed: () {
+              setState(() {
+                numOfItems++;
+              });
+            }),
+      ],
+    );
+  }
+
+  SizedBox buildOutlineButton({IconData icon, Function pressed}) {
+    return SizedBox(
+      width: 40,
+      height: 32,
+      child: OutlineButton(
+        onPressed: pressed,
+        padding: EdgeInsets.zero,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(13),
+        ),
+        child: Icon(icon),
       ),
     );
   }
